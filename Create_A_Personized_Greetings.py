6d640e1fa75c6728f6741be8d5ef3c664d665b5c
Karannnmmm <karannn.mmm@gmail.com>
2025-05-27 17:10:48 +0530

    Add files via upload

---
+++ Create_A_Personized_Greetings.py
@@ -0,0 +1,7 @@
+a=input('Enter your first name: ')
+b=input('Enter your last name: ')
+
+
+a1="Hello, "
+a2="! Welcome to the Python program"
+print(a1+a+" "+b+a2)
---
+++ Perform Basic Mathematical Operations.py
@@ -0,0 +1,12 @@
+a=input('Enter the first number: ')
+b=input('Enter the second number: ')
+a=int(a)
+b=int(b)
+Add=a+b
+print('Addition:',Add)
+Sub=a-b
+print('Subtraction:',Sub)
+Mult=a*b
+print('Multiplication:',Mult)
+Div=a/b
+print('Division:',Div)
